47ea2a383a8cc80827ed74f21561d60bf1c22618
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-10-30 20:57:45 +0100

    adding jest testing

---
+++ StarWars/src/components/HeroItem/HeroItem.test.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import HeroItem from "./HeroItem";
+import { searchHeroSlice } from "../../features/searchHeroSlice/searchHeroSlice";
+import heroesReducer from "../../features/allHeroesSlice/allHeroSlice";
+
+jest.mock("react-router-dom", () => ({
+  useNavigate: jest.fn(),
+}));
+
+describe("HeroItem Component", () => {
+  const mockHero = {
+    id: 21,
+    name: "Palpatine",
+    height: "170",
+    mass: "75",
+    hair_color: "grey",
+    skin_color: "pale",
+    eye_color: "yellow",
+    birth_year: "82BBY",
+    gender: "male",
+    homeworld: 8,
+    films: [2, 3, 4, 5, 6],
+    species: [1],
+    vehicles: [],
+    starships: [],
+    created: "2014-12-15T12:48:05.971000Z",
+    edited: "2014-12-20T21:17:50.347000Z",
+    url: "https://sw-api.starnavi.io/people/21/",
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const renderWithProvider = (children: React.ReactElement) => {
+    const store = configureStore({
+      reducer: {
+        allHeroes: heroesReducer,
+        [searchHeroSlice.reducerPath]: searchHeroSlice.reducer,
+      },
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(searchHeroSlice.middleware),
+    });
+
+    return render(<Provider store={store}>{children}</Provider>);
+  };
+
+  it("renders hero information correctly", () => {
+    renderWithProvider(<HeroItem hero={mockHero} />);
+
+    expect(screen.getByText(mockHero.name)).toBeInTheDocument();
+    expect(
+      screen.getByText(`Birth year: ${mockHero.birth_year}`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(`Gender: ${mockHero.gender}`)).toBeInTheDocument();
+  });
+
+  it("navigates to hero details on button click", () => {
+    const mockNavigate = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
+
+    renderWithProvider(<HeroItem hero={mockHero} />);
+
+    const button = screen.getByRole("button", { name: /Details/i });
+    fireEvent.click(button);
+
+    expect(mockNavigate).toHaveBeenCalledWith(`/StarWars/hero/${mockHero.id}`);
+  });
+});
---
+++ StarWars/src/components/HeroList/HeroList.test.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import "@testing-library/jest-dom";
+
+import HeroList from "./HeroList";
+import { fetchAllHeroes } from "../../features/allHeroesSlice/allHeroSlice";
+import { searchHeroSlice } from "../../features/searchHeroSlice/searchHeroSlice";
+
+const mockStore = (initialState: { allHeroes: any }) => {
+  return configureStore({
+    reducer: {
+      allHeroes: (state = initialState.allHeroes) => state,
+      [searchHeroSlice.reducerPath]: searchHeroSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(searchHeroSlice.middleware),
+  });
+};
+
+jest.mock("../../features/allHeroesSlice/allHeroesSlice", () => ({
+  fetchAllHeroes: jest.fn(() => ({
+    type: "allHeroes/fetchAllHeroes",
+  })),
+}));
+
+describe("HeroList", () => {
+  const initialState = {
+    allHeroes: {
+      heroes: [
+        { id: 1, name: "Superman" },
+        { id: 2, name: "Batman" },
+      ],
+      loading: false,
+      error: null,
+      next: true,
+      previous: false,
+    },
+  };
+
+  test("renders loading state", () => {
+    const store = mockStore({
+      allHeroes: {
+        ...initialState.allHeroes,
+        loading: true,
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <HeroList />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Loading.../)).toBeInTheDocument();
+  });
+
+  test("renders error state", () => {
+    const store = mockStore({
+      allHeroes: {
+        ...initialState.allHeroes,
+        error: "Some error occurred",
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <HeroList />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Error/)).toBeInTheDocument();
+  });
+
+  test("renders hero list", () => {
+    const store = mockStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <HeroList />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Page: 1/)).toBeInTheDocument();
+    expect(screen.getByText("Superman")).toBeInTheDocument();
+    expect(screen.getByText("Batman")).toBeInTheDocument();
+  });
+
+  test("handles pagination buttons", () => {
+    const store = mockStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <HeroList />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const nextButton = screen.getByText(/Next/);
+    const previousButton = screen.getByText(/Previous/);
+
+    expect(previousButton).toBeDisabled();
+    fireEvent.click(nextButton);
+    expect(fetchAllHeroes).toHaveBeenCalledWith(2);
+  });
+});
